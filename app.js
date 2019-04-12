af3b24beef5aba0364e5dab61de26d61767a3dd5
Campbell Cole <cmcoleco@gmail.com>
2019-04-11 19:10:21 -0700

    locklocklocklocklocklocklock

--- app.js
+++ app.js
@@ -4,16 +4,37 @@ var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var fs = require('fs')
 
-var tempCounter = 1
-
 app.use(express.static('public'))
 
+function isTraining(callback) {
+  fs.exists('private/processing.lock', (exists) => callback(exists))
+}
+
 io.on('connection', (socket) => {
-  socket.on('testdata', (dat) => {
+  socket.on('upload data', (dat) => {
     console.log(`received data. saving...`)
-    fs.writeFile(`uploads/file${tempCounter++}.test`, dat, (err) => {
-      if (err) throw err
-      console.log('saved')
+    isTraining((training) => {
+      if (!training) {
+        fs.writeFile('private/vid.mp4', dat, (err) => {
+          if (err) throw err
+          console.log('saved')
+          socket.emit('saved')
+        })
+      } else {
+        socket.emit('alert', 'already training. not gonna save the file.')
+      }
+    })
+  })
+  socket.on('dream', () => {
+    isTraining((training) => {
+      if (training) socket.emit('alert', 'already training. this button is useless now.')
+      else {
+        fs.writeFile('private/processing.lock', 0xDEADBEEFDEADBEEF, (err) => {
+          if (err) throw err
+          console.log('locked...')
+          training = true
+        })
+      }
     })
   })
   console.log('connected')
--- public/client.js
+++ public/client.js
@@ -1,10 +1,13 @@
+var socket = io();
+
 window.addEventListener('load', ready)
 
 function ready() {
-  if (!window.File || !window.FileReader) {
-    alert('update your browser man it\'s not that hard')
-  } else {
-    console.log('good to go babyyyyy')
+  document.getElementById('inp').addEventListener('change', chosen)
+  document.getElementById('submit').onclick = submit
+
+  document.getElementById('hit_that_yeet').onclick = function() {
+    socket.emit('dream')
   }
 }
 
@@ -13,18 +16,26 @@ var didChoose = false
 
 function chosen(event) {
   file = event.target.files[0]
-  didChoose = true
+  if (file.name.split('.')[file.name.split('.').length-1] != "mp4") { // don't do this kids...
+    alert('wrong file type. you gotta use .mp4 holmes.')
+    didChoose = false
+    file = null
+  } else {
+    didChoose = true
+  }
 }
 
 function submit() {
   if (didChoose) {
-    socket.emit('testdata', file)
+    socket.emit('upload data', file)
   } else {
     alert('choose a file first dummy')
   }
 }
 
-document.getElementById('inp').addEventListener('change', chosen)
-document.getElementById('submit').onclick = submit
+socket.on('saved', () => {
+  console.log('BOP yeah aight, blueface baby. let\'s get dreaming.')
+  document.getElementById('hit_that_yeet').hidden = false
+})
 
-var socket = io();
+socket.on('alert', (msg) => alert(msg))
